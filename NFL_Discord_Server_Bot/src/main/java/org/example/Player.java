3f60488eb2f53c1a71aa54d07bd44cb0d807c6d4
matthoot <matt221136@gmail.com>
2023-11-16 14:37:15 -0500

    fixed player stat formatting for QBs

--- NFL_Discord_Server_Bot/src/main/java/org/example/Player.java
+++ NFL_Discord_Server_Bot/src/main/java/org/example/Player.java
@@ -9,8 +9,7 @@ import org.jsoup.select.Elements;
 
 public class Player {
 
-    public static void getPlayerInfo(String playerName) {
-
+    public static void getQBInfo(String playerName) {
         try {
             String formattedUrl = formatPlayerUrl(playerName);
             // Set a timeout for the connection (e.g., 5000 milliseconds)
@@ -20,36 +19,38 @@ public class Player {
             Element tbody = doc.select("table.statistics tbody").first();
 
             if (tbody != null) {
-                // Select all rows within the tbody
+                // Select the third-to-last row within the tbody
                 Elements rows = tbody.select("tr");
+                int thirdToLastRowIndex = rows.size() - 3;
 
-                // Display headers
-                displayHeaders();
+                if (thirdToLastRowIndex >= 0) {
+                    Element secondToLastRow = rows.get(thirdToLastRowIndex);
 
-                // Iterate through rows and display player stats
-                for (Element row : rows) {
-                    // Select all cells within the row
-                    Elements cells = row.select("td");
+                    // Select all cells within the second-to-last row
+                    Elements cells = secondToLastRow.select("td");
 
-                    // Display each cell's text content
-                    for (int i = 0; i < cells.size(); i++) {
-                        String cellText = cells.get(i).text();
+                    int year = Integer.parseInt(cells.get(0).text());
 
-                        // For the "Team" column, extract the acronym
-                        if (i == 1) {
-                            String[] teamNameParts = cellText.split("\\s");
-                            System.out.print(teamNameParts[teamNameParts.length - 1] + "\t\t");
-                        } else {
-                            System.out.print(cellText + "\t\t");
-                        }
-                    }
+                    String fullTeam = cells.get(1).text();
+                    String team = extractTeamInitials(fullTeam);
 
-                    // Move to the next line after processing each row
-                    System.out.println();
+                    double completionPercentage = Double.parseDouble(cells.get(7).text());
+
+                    String yardsWithCommas = cells.get(8).text();
+                    String yardsWithoutCommas = yardsWithCommas.replace(",", "");
+                    int yards = Integer.parseInt(yardsWithoutCommas);
+
+                    int passingTouchdowns = Integer.parseInt(cells.get(11).text());
+                    int interceptions = Integer.parseInt(cells.get(13).text());
+
+                    System.out.println("year = " + year);
+                    System.out.println("team = " + team);
+                    System.out.println("completion percentage = " + completionPercentage);
+                    System.out.println("yards = " + yards);
+                    System.out.println("passing touchdowns =  " + passingTouchdowns);
+                    System.out.println("interceptions = " + interceptions);
                 }
             }
-
-
         } catch (IOException e) {
             // Handle IOException (e.g., network issues, invalid URL)
             e.printStackTrace();
@@ -59,10 +60,16 @@ public class Player {
         }
     }
 
-    private static void displayHeaders() {
-        System.out.println("Year\t\tTeam\t\tLeague\tG\tGS\tAtt\tCmp\tPct\tYds\tYPA\tYPG\tTD\tTD%\tInt\tInt%\tLg\tFD\t20+\tSack\tLoss\tRate");
+    public static String extractTeamInitials(String input){
+        // Iterate over the last 3 characters and check if they are all uppercase
+        for (int i = input.length() - 3; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (!Character.isUpperCase(currentChar)) {
+                return input.substring(input.length() - 2); // If any character is not uppercase, return last two digits
+            }
+        }
+        return input.substring(input.length() - 3); // else return last three digits
     }
-
     public static String formatPlayerUrl(String playerName) {
         // Convert the player name to lowercase
         String lowercaseName = playerName.toLowerCase();
@@ -70,9 +77,14 @@ public class Player {
         // Replace spaces with hyphens
         String hyphenatedName = lowercaseName.replace(" ", "-");
 
-        // Extract last name and first two letters of the first name
+        // Extract first five letters of last name and first two letters of the first name
         String[] nameParts = lowercaseName.split(" ");
         String lastName = nameParts[nameParts.length - 1];
+        // Ensure that the last name has at least 5 characters before extracting
+        if (lastName.length() > 5) {
+            // Extract the first 5 letters
+            lastName = lastName.substring(0, 5);
+        }
         String firstNameAbbreviation = nameParts[0].substring(0, Math.min(2, nameParts[0].length()));
 
         // Generate the unique identifier
@@ -80,13 +92,18 @@ public class Player {
 
         // Build the formatted URL
         String formattedUrl = "https://www.footballdb.com/players/" + hyphenatedName + "-" + uniqueIdentifier;
-
+        System.out.println(formattedUrl);
         return formattedUrl;
     }
 
     public static void main(String[] args) {
+    //Get player position
+    //QB, Receiver, Defensive Player, Kicker, Punter, OL(other?)
+        getQBInfo("Jalen Hurts");
+        getQBInfo("Sam Howell");
+        getQBInfo("Daniel Jones");
+        getQBInfo("Dak Prescott");
 
-        String playerName = "Jalen Hurts"; // Replace with the actual player name
-        getPlayerInfo(playerName);
+
     }
 }
