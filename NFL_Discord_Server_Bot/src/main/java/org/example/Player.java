4f5295fb0478ab90df4b28b382de561a7c618046
matthoot <matt221136@gmail.com>
2023-11-27 21:20:07 -0500

    finished logic to sort through valid URLs for valid players

--- NFL_Discord_Server_Bot/src/main/java/org/example/Player.java
+++ NFL_Discord_Server_Bot/src/main/java/org/example/Player.java
@@ -9,108 +9,147 @@ import org.jsoup.select.Elements;
 
 public class Player {
 
-    public static void getPlayerInfo(String playerName) {
+    public static void findPlayer(String playerName){
 
-        String url = formatPlayerUrl(playerName);
+        int i = 1;
+        boolean foundPlayer = false;
+        String url = "";
+        String retrievedName = "name";   
+
         try {
-            Document doc = Jsoup.connect(url).get();
-            String position = getPosition(doc);
-            String team = getTeam(doc);
-            Element statTable = getStatTable(url, doc, position);
-            Elements lastRow = getLastRow(statTable);
+            while (!retrievedName.equals("")){
+                //if the retrieved name matches 
+                url = formatPlayerUrl(playerName, i);
+                Document doc = Jsoup.connect(url).get();
+                retrievedName = getRetrievedName(doc);
 
-            System.out.println("team = " + team);
-            System.out.println("position = " + position);
-
-
-            if (position.equals("QB")){
-                //completion percentage, passing yards, passing touchdowns, and interceptions
-                double completionPercentage = Double.parseDouble(lastRow.get(7).text());
-
-                String yardsWithCommas = lastRow.get(8).text();
-                String yardsWithoutCommas = yardsWithCommas.replace(",", "");
-                int yards = Integer.parseInt(yardsWithoutCommas);
-                int passingTouchdowns = Integer.parseInt(lastRow.get(11).text());
-                int interceptions = Integer.parseInt(lastRow.get(13).text());  
-
-                System.out.println("completion percentage = " + completionPercentage);
-                System.out.println("yards = " + yards);
-                System.out.println("passing touchdowns =  " + passingTouchdowns);
-                System.out.println("interceptions = " + interceptions);
-
-            } else if (position.equals("WR") || position.equals("TE")) {
-                //receptions, yards, touchdowns
-                int receptions = Integer.parseInt(lastRow.get(5).text());
-                String yardsWithCommas = lastRow.get(6).text();
-                String yardsWithoutCommas = yardsWithCommas.replace(",", "");
-                int yards = Integer.parseInt(yardsWithoutCommas);
-                int touchdowns = Integer.parseInt(lastRow.get(10).text());
-
-                System.out.println("yards = " + yards);
-                System.out.println("receptions = " + receptions);
-                System.out.println("touchdowns = " + touchdowns);
-
-
-            } else if (position.equals("RB")) {
-                //Attempts, Yards, Touchdowns
-                int attempts = Integer.parseInt(lastRow.get(5).text());
-
-                String yardsWithCommas = lastRow.get(6).text();
-                String yardsWithoutCommas = yardsWithCommas.replace(",", "");
-                int yards = Integer.parseInt(yardsWithoutCommas);
-                int touchdowns = Integer.parseInt(lastRow.get(10).text());
-
-                System.out.println("attempts = " + attempts);
-                System.out.println("yards = " + yards);
-                System.out.println("touchdowns = " + touchdowns);
-
-
-            //Defensive player
-            } else if (position.equals("LB") || position.equals("DB") || position.equals("LB") ||
-             position.equals("DT") || position.equals("DE")) {
-                //total tackles, sacks, interceptions, pass deflected
-
-                int totalTackles = Integer.parseInt(lastRow.get(12).text());
-                double sacks = Double.parseDouble(lastRow.get(13).text());
-                int passDeflected = Integer.parseInt(lastRow.get(15).text());
-                int interceptions = Integer.parseInt(lastRow.get(5).text());
-
-                System.out.println("total tackles = " + totalTackles);
-                System.out.println("sacks = " + sacks);
-                System.out.println("passes deflected = " + passDeflected);
-                System.out.println("interceptions = " + interceptions);
-
-            //Kicker
-            } else if (position.equals("K")) {
-
-                //field goal percentage , extra point percentage, longest kick
-                double fieldGoalPercentage = Double.parseDouble(lastRow.get(7).text());
-                int longestMade = Integer.parseInt(lastRow.get(13).text());
-                double extraPointPercentage = Double.parseDouble(lastRow.get(16).text());
-
-                System.out.println("field goal percentage = " + fieldGoalPercentage + "%");
-                System.out.println("extra point percentage = " + extraPointPercentage + "%");
-                System.out.println("longest made field goal = " + longestMade);
-
-            //Punter
-            } else if (position.equals("P")) {
-                //average punt yards, longest, inside the 20
-                //7  8 10
-
-                double averageYards = Double.parseDouble(lastRow.get(7).text());
-                int longestPunt = Integer.parseInt(lastRow.get(8).text());
-                int inside20 = Integer.parseInt(lastRow.get(10).text());
-
-                System.out.println("average punt yards = " + averageYards);
-                System.out.println("longest punt = " + longestPunt);
-                System.out.println("punts inside the 20 = " + inside20);
-
+                if (retrievedName.equals(playerName)){
+                    getPlayerInfo(playerName, url, doc);
+                    foundPlayer = true;
+                }
+                
+                i++;
             }
-
-        } catch (IOException e){
+            if (!foundPlayer){
+                System.out.println("Player not found.");
+            }
+            
+            //while the URL is valid
+            
+        }  catch (IOException e){
             e.printStackTrace();
         }
+        
+        
+    
+
+        
+        
+        
     }
+
+    public static void getPlayerInfo(String playerName, String url, Document doc) {
+
+        String position = getPosition(doc);
+        String team = getTeam(doc);
+        String retreivedName = getRetrievedName(doc);
+        Element statTable = getStatTable(url, doc, position);
+        if (statTable == null) {
+            System.out.println("not enough info found");
+            return;
+        }
+        Elements lastRow = getLastRow(statTable);
+
+        System.out.println("team = " + team);
+        System.out.println("position = " + position);
+        System.out.println("name = " + retreivedName);
+
+        
+
+        if (position.equals("QB")){
+            //completion percentage, passing yards, passing touchdowns, and interceptions
+            double completionPercentage = Double.parseDouble(lastRow.get(7).text());
+
+            String yardsWithCommas = lastRow.get(8).text();
+            String yardsWithoutCommas = yardsWithCommas.replace(",", "");
+            int yards = Integer.parseInt(yardsWithoutCommas);
+            int passingTouchdowns = Integer.parseInt(lastRow.get(11).text());
+            int interceptions = Integer.parseInt(lastRow.get(13).text());  
+
+            System.out.println("completion percentage = " + completionPercentage);
+            System.out.println("yards = " + yards);
+            System.out.println("passing touchdowns =  " + passingTouchdowns);
+            System.out.println("interceptions = " + interceptions);
+
+        } else if (position.equals("WR") || position.equals("TE")) {
+            //receptions, yards, touchdowns
+            int receptions = Integer.parseInt(lastRow.get(5).text());
+            String yardsWithCommas = lastRow.get(6).text();
+            String yardsWithoutCommas = yardsWithCommas.replace(",", "");
+            int yards = Integer.parseInt(yardsWithoutCommas);
+            int touchdowns = Integer.parseInt(lastRow.get(10).text());
+            
+            System.out.println("yards = " + yards);
+            System.out.println("receptions = " + receptions);
+            System.out.println("touchdowns = " + touchdowns);
+
+
+        } else if (position.equals("RB")) {
+            //Attempts, Yards, Touchdowns
+            int attempts = Integer.parseInt(lastRow.get(5).text());
+
+            String yardsWithCommas = lastRow.get(6).text();
+            String yardsWithoutCommas = yardsWithCommas.replace(",", "");
+            int yards = Integer.parseInt(yardsWithoutCommas);
+            int touchdowns = Integer.parseInt(lastRow.get(10).text());
+        
+            System.out.println("attempts = " + attempts);
+            System.out.println("yards = " + yards);
+            System.out.println("touchdowns = " + touchdowns);
+
+
+        //Defensive player
+        } else if (position.equals("LB") || position.equals("DB") || position.equals("LB") ||
+            position.equals("DT") || position.equals("DE")) {
+            //total tackles, sacks, interceptions, pass deflected
+
+            int totalTackles = Integer.parseInt(lastRow.get(12).text());
+            double sacks = Double.parseDouble(lastRow.get(13).text());
+            int passDeflected = Integer.parseInt(lastRow.get(15).text());
+            int interceptions = Integer.parseInt(lastRow.get(5).text());
+          
+            System.out.println("total tackles = " + totalTackles);
+            System.out.println("sacks = " + sacks);
+            System.out.println("passes deflected = " + passDeflected);
+            System.out.println("interceptions = " + interceptions);
+
+        //Kicker
+        } else if (position.equals("K")) {
+
+            //field goal percentage , extra point percentage, longest kick
+            double fieldGoalPercentage = Double.parseDouble(lastRow.get(7).text());
+            int longestMade = Integer.parseInt(lastRow.get(13).text());
+            double extraPointPercentage = Double.parseDouble(lastRow.get(16).text());
+          
+            System.out.println("field goal percentage = " + fieldGoalPercentage + "%");
+            System.out.println("extra point percentage = " + extraPointPercentage + "%");
+            System.out.println("longest made field goal = " + longestMade);
+
+        //Punter
+        } else if (position.equals("P")) {
+            //average punt yards, longest, inside the 20
+
+            double averageYards = Double.parseDouble(lastRow.get(7).text());
+            int longestPunt = Integer.parseInt(lastRow.get(8).text());
+            int inside20 = Integer.parseInt(lastRow.get(10).text());
+            
+            System.out.println("average punt yards = " + averageYards);
+            System.out.println("longest punt = " + longestPunt);
+            System.out.println("punts inside the 20 = " + inside20);
+        }
+
+    }   
+    
 
     public static Element getStatTable(String url, Document doc, String position) {
         // Select the table body containing player stats
@@ -138,15 +177,9 @@ public class Player {
         } else {
             //select defense table
             statTable = doc.select("#divToggle_D_reg > table.statistics tbody").first();
-
         }
 
-        if (statTable == null) {
-            System.out.println("failed to fetch player stat table.");
-            return null;
-        } else {
-            return statTable;
-        }
+        return statTable;
     }
 
     public static String getPosition(Document doc){
@@ -183,11 +216,16 @@ public class Player {
                 // Get the text content of the next sibling, which contains the position
                 return positionElement.text();
             } else {
-                return "Position element not found";
+                return "team not found";
             }
         } else {
-            return "Player banner element not found";
+            return "team not found";
         }
+    }
+
+    public static String getRetrievedName(Document doc){
+        Element positionElement =  doc.select("#playertop > h1").first();
+        return positionElement.text();
     }
 
     public static Elements getLastRow(Element statTable) {
@@ -204,7 +242,7 @@ public class Player {
     }
 
 
-    public static String formatPlayerUrl(String playerName) {
+    public static String formatPlayerUrl(String playerName, int i) {
 
         // Remove periods from players name
         String nameWithoutPeriods = playerName.replace(".", "");
@@ -223,9 +261,13 @@ public class Player {
             lastName = lastName.substring(0, 5);
         }
         String firstNameAbbreviation = nameParts[0].substring(0, Math.min(2, nameParts[0].length()));
-
+        String uniqueIdentifier = "";
         // Generate the unique identifier
-        String uniqueIdentifier = lastName + firstNameAbbreviation + "01";
+        if (i < 10){
+            uniqueIdentifier = lastName + firstNameAbbreviation + "0" + i;
+        } else {
+            uniqueIdentifier = lastName + firstNameAbbreviation + i;
+        }
 
         // Build the formatted URL
         String formattedUrl = "https://www.footballdb.com/players/" + hyphenatedName + "-" + uniqueIdentifier;
@@ -234,16 +276,17 @@ public class Player {
     }
 
     public static void main(String[] args) {
-        // getPlayerInfo("Jalen Hurts");
-        // getPlayerInfo("Haason Reddick");
-        // getPlayerInfo("Darius Slay");
-        // getPlayerInfo("A.J. Brown");
-        // getPlayerInfo("Travis Kelce");
-        // getPlayerInfo("Christian McCaffrey");
-        // getPlayerInfo("Braden Mann");
-        // getPlayerInfo("Jason Kelce");
-
-        // getPlayerInfo("Lane Johnson");
+        findPlayer("Jalen Hurts");
+        findPlayer("Haason Reddick");
+        findPlayer("Darius Slay");
+        findPlayer("A.J. Brown");
+        findPlayer("Travis Kelce");
+        findPlayer("Christian McCaffrey");
+        findPlayer("Jake Elliott");
+        findPlayer("Braden Mann");
+        findPlayer("Jason Kelce");
+        findPlayer("Josh Allen");
 
     }
+
 }
